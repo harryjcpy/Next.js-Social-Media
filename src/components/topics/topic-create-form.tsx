d782f7da23dacdcf19433714e9edfc2b847e5adc
harryjcpy <harsheit.mishra@gmail.com>
2024-11-30 20:11:36 +0530

    added validations in the form element

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useFormState } from "react-dom";
 import {
     Input,
     Button,
@@ -9,17 +12,33 @@ import {
 import * as actions from '@/actions';
 
 export default function TopicCreateForm() {
+    const [formState, action] = useFormState(actions.createTopic, {
+        errors: {}
+    });
+
     return (
         <Popover>
             <PopoverTrigger>
                 <Button color="primary">Create a Topic</Button>
             </PopoverTrigger>
             <PopoverContent>
-                <form action={actions.createTopic}>
+                <form action={action}>
                     <div className="flex flex-col gap-4 p-4 w-80">
                         <h3 className="text-lg">Create a Topic</h3>
-                        <Input name="name" label="Name" labelPlacement="outside" placeholder="Name" />
-                        <Textarea name="description" label="Description" labelPlacement="outside" placeholder="Describe your topic" />
+                        <Input name="name" 
+                        label="Name" 
+                        labelPlacement="outside" 
+                        placeholder="Name" 
+                        isInvalid={!!formState.errors.name}
+                        errorMessage={formState.errors.name?.join(', ')} />
+                        <Textarea name="description" 
+                        label="Description" 
+                        labelPlacement="outside" 
+                        placeholder="Describe your topic"
+                        isInvalid={!!formState.errors.description}
+                        errorMessage={formState.errors.description?.join(', ')}
+                        />
+                        {formState.errors._form ? <div className="rounded p-2 bg-red-200 border border-red-400">{formState.errors._form?.join(', ')}</div> : null}
                         <Button type="submit" color="warning">Submit</Button>
                     </div>
                 </form>
